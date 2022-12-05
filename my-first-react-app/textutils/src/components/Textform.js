e3148d9a0689884c6f324658d1700bbdf3af4a85
Sarthak Zalte <sarthakzalte@gmail.com>
2022-12-05 14:21:28 +0530

    Added two functions Clear Text & Remove extra spaces

--- my-first-react-app/textutils/src/components/Textform.js
+++ my-first-react-app/textutils/src/components/Textform.js
@@ -16,6 +16,33 @@ export default function Textform(props) {
     setText(newText);
   }
 
+  // function to clear the text field area
+  const handleClearClick = ()=>{
+    console.log("Clear-click was clicked");
+    let newText = "";
+    setText(newText);
+  }
+
+  // function to copy the content in text field area
+  const handleCopyClick = ()=>{
+    console.log("Copy-click was clicked");
+
+    //to select the items in the text field
+    let copyText = document.getElementById("myBox");
+    copyText.select();
+
+    //main work 
+    navigator.clipboard.writeText(copyText.value);
+    console.log("copied text: " +copyText.value);
+
+  }
+
+  // function to remove extra spaces
+  const handleExtraSpaces = () => {
+    let newText = text.split(/[ ]+/);
+    setText(newText.join(" "))
+  }
+
   // function to handle on change
   const handleOnchange = (event)=>{
     console.log("on change")
@@ -33,12 +60,14 @@ export default function Textform(props) {
             </div>
             <button className="btn btn-primary mx-1" onClick={handleUpClick}>Convert to Uppercase</button>
             <button className="btn btn-primary mx-1" onClick={handleLoClick}>Convert to Lowercase</button>
+            <button className="btn btn-primary mx-1" onClick={handleClearClick}>Clear text</button>
+            <button className="btn btn-primary mx-1" onClick={handleExtraSpaces}>Remove Extra Spaces</button>
       </div>
       <div className="container my-4">
             <h1>Your Text Summary</h1>
             {/* text.split(" ") -- function gives the array of words and length of that array is equal to words */}
             <p> {text.split(" ").length} words & {text.length} characters</p>
-            <p>{0.008 * text.split(" ").length} Minutes saved to Analyse the text</p>
+            <p>{(0.008 * text.split(" ").length)*60} seconds saved to Analyse the text</p>
             <h2>Preview</h2>
             <p>{text}</p>
       </div>
