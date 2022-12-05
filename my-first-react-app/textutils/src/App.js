f8db2657b83d8e2097a02679e53aa8cf0263b4cb
Sarthak Zalte <sarthakzalte@gmail.com>
2022-12-05 17:54:40 +0530

    Added a dark mode button using hooks

--- my-first-react-app/textutils/src/App.js
+++ my-first-react-app/textutils/src/App.js
@@ -1,14 +1,16 @@
-import './App.css';
-import Navbar from './components/Navbar';
-import Textform from './components/Textform';
+import "./App.css";
+import About from "./components/About";
+import Navbar from "./components/Navbar";
+import Textform from "./components/Textform";
 
 function App() {
   return (
     <>
-<Navbar title = "TextUtils"  aboutText = "About Textutils" />
-<div className="container my-3"> 
-<Textform  heading= "Enter Text to Analyse"/>
-</div>
+      <Navbar title="TextUtils" aboutText="About Textutils" />
+      <div className="container my-3">
+        {/* <Textform  heading= "Enter Text to Analyse"/> */}
+        <About />
+      </div>
     </>
   );
 }
