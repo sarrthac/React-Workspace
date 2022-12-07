c85757e35b2fa8e190dc9ea0ca05338e1d7b9fb9
Sarthak Zalte <sarthakzalte@gmail.com>
2022-12-07 10:39:19 +0530

    Enhanced the dark mode feature and made it for full page - 12

--- my-first-react-app/textutils/src/App.js
+++ my-first-react-app/textutils/src/App.js
@@ -1,15 +1,27 @@
+import { useState } from "react";
 import "./App.css";
-import About from "./components/About";
 import Navbar from "./components/Navbar";
 import Textform from "./components/Textform";
 
 function App() {
+
+  const[mode,setMode] = useState('light')  //Wheather light mode is enabled or not
+
+  const toggleMode = () => {
+    if( mode === 'light'){
+      setMode('dark');
+      document.body.style.backgroundColor = '#464b50';
+    }else{
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+    }
+  }
   return (
     <>
-      <Navbar title="TextUtils" aboutText="About Textutils" />
+      <Navbar title="TextUtils" aboutText="About Textutils"  toggleMode={toggleMode} mode={mode}/>
       <div className="container my-3">
-        {/* <Textform  heading= "Enter Text to Analyse"/> */}
-        <About />
+        <Textform  heading= "Enter Text to Analyze below" mode ={mode} />
+        {/* <About /> */}
       </div>
     </>
   );
--- my-first-react-app/textutils/src/components/Textform.js
+++ my-first-react-app/textutils/src/components/Textform.js
@@ -53,23 +53,23 @@ export default function Textform(props) {
   const [text,setText] = useState('Enter text here');
   return (
     <>
-      <div className="container"> 
+      <div className="container" style={{color: props.mode==='dark'?'white':'#464b50'}}> 
             <h1>{props.heading}</h1>
             <div className="mb-3"> 
-            <textarea className="form-control" value={text} onChange={handleOnchange} id="myBox" rows="8"></textarea>
+            <textarea className="form-control" value={text} onChange={handleOnchange} style={{backgroundColor: props.mode ==='dark'?'#464b50':'white', color: props.mode==='dark'?'white':'black'}} id="myBox" rows="8"></textarea>
             </div>
             <button className="btn btn-primary mx-1" onClick={handleUpClick}>Convert to Uppercase</button>
             <button className="btn btn-primary mx-1" onClick={handleLoClick}>Convert to Lowercase</button>
             <button className="btn btn-primary mx-1" onClick={handleClearClick}>Clear text</button>
             <button className="btn btn-primary mx-1" onClick={handleExtraSpaces}>Remove Extra Spaces</button>
       </div>
-      <div className="container my-4">
+      <div className="container my-4" style={{color: props.mode==='dark'?'white':'#464b50'}}>
             <h1>Your Text Summary</h1>
             {/* text.split(" ") -- function gives the array of words and length of that array is equal to words */}
             <p> {text.split(" ").length} words & {text.length} characters</p>
             <p>{(0.008 * text.split(" ").length)*60} seconds saved to Analyse the text</p>
             <h2>Preview</h2>
-            <p>{text}</p>
+            <p>{text.length>0?text:"Enter something in the textbox above to preview it here"}</p>
       </div>
     </>
   )
