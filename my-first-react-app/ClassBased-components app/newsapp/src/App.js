a417ffa5e1428eff407799e763a43aaf27a97d66
Sarthak Zalte <sarthakzalte@gmail.com>
2022-12-19 13:35:10 +0530

    completed Looping through an array in JSX to display NewsItems from state

--- my-first-react-app/ClassBased-components app/newsapp/src/App.js
+++ my-first-react-app/ClassBased-components app/newsapp/src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
 import NavBar from './Components/NavBar';
+import News from './Components/News';
 
 function App() {
   return (
+    <div>
     <NavBar />
+    <News />
+    </div>
   );
 }
 
 export default App;
+
+//api key - a21c1b7798e84923acd119b32769d327
