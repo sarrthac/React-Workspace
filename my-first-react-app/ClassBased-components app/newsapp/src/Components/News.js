135dee9832e61221f857957227aab5ccf4ba0cdd
Sarthak Zalte <sarthakzalte@gmail.com>
2022-12-19 16:07:15 +0530

    Added an API end point to fetch the news

--- my-first-react-app/ClassBased-components app/newsapp/src/Components/News.js
+++ my-first-react-app/ClassBased-components app/newsapp/src/Components/News.js
@@ -113,7 +113,24 @@ export class News extends Component {
       articles : this.articles,
     }
   };
+
+  async componentDidMount(){
+      console.log("cdm");
+
+      //url is the endpoint to get top headline news of india 
+      let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=a21c1b7798e84923acd119b32769d327";
+      let data = await fetch(url);
+      let parsedData = await data.json();
+      console.log(parsedData);
+
+      //setting the state
+      this.setState({articles: parsedData.articles})
+
+  }
+
+
   render() {
+    console.log("Render")
     return (
       <div className='container my-3'>
         <h2>News-Katta - Top Headlines</h2>
