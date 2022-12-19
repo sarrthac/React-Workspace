d0011994d4cee4a02e416b204a630eeccb1c7590
Sarthak Zalte <sarthakzalte@gmail.com>
2022-12-19 17:57:01 +0530

    Added Previous & Next Buttons to populate NewsItems

--- my-first-react-app/ClassBased-components app/newsapp/src/Components/News.js
+++ my-first-react-app/ClassBased-components app/newsapp/src/Components/News.js
@@ -8,6 +8,8 @@ export class News extends Component {
     super();
     this.state = {
       articles : [],
+      loading : false,
+      page : 1
     }
   };
 
@@ -21,10 +23,40 @@ export class News extends Component {
       console.log(parsedData);
 
       //setting the state
-      this.setState({ articles: parsedData.articles})
+      this.setState({ articles: parsedData.articles, totalResults: parsedData.totalResults})
 
   }
 
+
+  // In the below code we have used the if-else statement to use the function 
+  // only if the next page exists otherwise it won’t be executed. Now if the
+  //  function is executed then the state of the page will be updated by 1.
+  handleNextClick = async() => {
+    console.log("next Button");
+    if(this.state.page + 1 > Math.ceil(this.state.totalResults / 20)){
+       //meaning - if the page number is greater than the Math.ceil(this.state.totalResults / 20) then nothing will happen
+    }else{
+      let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=a21c1b7798e84923acd119b32769d327&page=${this.state.page + 1}&pageSize=20`;
+      let data = await fetch(url);
+      let parsedData = await data.json();
+      this.setState({
+        page: this.state.page + 1,
+        articles: parsedData.articles
+        })
+    }
+  }
+
+  handlePrevClick = async () => {
+    console.log("Previous");
+    let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=a21c1b7798e84923acd119b32769d327&page=${this.state.page - 1}&pageSize=20`;
+    let data = await fetch(url);
+    let parsedData = await data.json()
+    console.log(parsedData);
+    this.setState({
+        page: this.state.page - 1,
+        articles: parsedData.articles
+    })
+}
 
   render() {
     console.log("Render")
@@ -37,7 +69,10 @@ export class News extends Component {
             <NewsItems  title = {element.title?element.title.slice(0,45):""}  imageUrl={element.urlToImage} description = {element.description?element.description.slice(0,88):""} newsUrl = {element.url}/>
             </div>
           })}
-         
+        </div>
+        <div className="container d-flex justify-content-between">
+         <button type='button' button disabled={this.state.page<=1} className='btn btn-light' onClick={this.handlePrevClick} > &larr; Previous</button>
+         <button type='button' button disabled={this.state.page + 1 > Math.ceil(this.state.totalResults / 20)} className='btn btn-light' onClick={this.handleNextClick}>  Next &rarr;</button> 
         </div>
       </div>
     )
