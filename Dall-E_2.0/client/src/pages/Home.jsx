d0fe00140330e505d2d0598c0e42b6de8066aa6e
Sarthak Zalte <sarthakzalte@gmail.com>
2023-08-28 12:38:12 +0530

    Created and updated the Home.jsx

--- Dall-E_2.0/client/src/pages/Home.jsx
+++ Dall-E_2.0/client/src/pages/Home.jsx
@@ -2,10 +2,23 @@ import React,{useState,useEffect} from 'react';
 import {Loader,Card,FormField} from '../components';
 
 function Home() {
+
+  const RenderCards = ({data,title}) => {
+    if(data?.length > 0) {
+      return data.map((post) => <Card key={post._id}{...post} />)
+    }
+
+    return (
+      <h2 className='mt-5 font-bold text-[#6449ff] text-xl uppercase'>
+        {title}
+      </h2>
+    )
+  }
+
   const [loading, setLoading] = useState(false);
   const [allPosts, setAllPosts] = useState(null);
 
-  const [searchText, setSearchText] = useState('abc');
+  const [searchText, setSearchText] = useState('');
 
   return (
     <section>
@@ -30,7 +43,21 @@ function Home() {
                 <h2 className='font-medium text-[#666e75] text-xl mb-3'>
                   Showing results for <span className='text-[#222328]'>{searchText}</span>
                 </h2>
-              ) }
+              )}
+              <div className='grid lg:grid-cols-4 sm:grid-cols-3 xs:grid-cols-2 grid-cols-1 gap-3'>
+                  {searchText ? (
+                    <RenderCards 
+                      data={[]}
+                      title= "No search results found"
+                    />
+                  ): (
+                    <RenderCards 
+                      data={[]}
+                      title="No posts found"
+                    />
+                  )
+                }
+              </div>
             </>
           )
         }
