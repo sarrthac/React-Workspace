5832e304d6059cb2a1001e1b485ebf44b1a89fc5
Sarthak Zalte <sarthakzalte@gmail.com>
2024-04-29 15:27:10 +0530

    Added CreatePost page

--- Dall-E_2.0/client/src/utils/index.js
+++ Dall-E_2.0/client/src/utils/index.js
@@ -1,12 +1,12 @@
 import { surpriseMePrompts } from "../constants";
 
-export function getRadomPrompt(prompt){
+export function getRandomPrompt(prompt){
 
     const randomIndex = Math.floor(Math.random() * surpriseMePrompts.length);
     const randomPrompt = surpriseMePrompts[randomIndex];
 
     //check to ensure we do not get same surpriseMePrompts in a row
-    if(randomPrompt === prompt) return getRadomPrompt(prompt);
+    if(randomPrompt === prompt) return getRandomPrompt(prompt);
 
     return randomPrompt;
-}
+} 
