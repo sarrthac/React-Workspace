96fb6f3bed557fc7961616e706ef0fc0b1e7df18
Sarthak Zalte <sarthakzalte@gmail.com>
2024-08-02 16:31:09 +0530

    Rendering of all products completed

--- pagination/readme.txt
+++ pagination/readme.txt
@@ -1,5 +1,20 @@
 Concepts - 
 
+WEB(HTML<CSS<OTHERS)
+1)span<> tag
+2)Importance of alt in img<> tag
+3)Bam convention - A way to write css classes
+4)While mapping products using key for react virtual DOM to differencitate between these products.
+5)Css Grid
+
+
+JavaScript - 
+
 1) Async & Await Keywords 
-2) Fetch vs Axios
-3) UseEffect() hook 
+2) Fetch vs Axios 
+3) map function of arrays 
+4) 
+ 
+
+React - 
+1) UseEffect() hook 
--- pagination/src/App.js
+++ pagination/src/App.js
@@ -20,9 +20,20 @@ function App() {
   }, []) 
   
   return (
-    <div className="App">
-  
-    </div>
+   <div>
+      {products.length > 0 && (
+        <div className='products'>
+          {products.map((prod) => {
+            return(
+              <span className='products__single' key={prod.id}>
+                <img src={prod.thumbnail} alt={prod.title}/>
+                <span>{prod.title}</span>
+              </span>
+            );
+          })}
+        </div>
+      )};
+   </div>
   );
 }
 
