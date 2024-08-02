4e40dcef8520dadd284a1c8e32552d2ca5e98678
Sarthak Zalte <sarthakzalte@gmail.com>
2024-08-02 15:57:58 +0530

    Fetch products data -

---
+++ pagination/readme.txt
@@ -0,0 +1,5 @@
+Concepts - 
+
+1) Async & Await Keywords 
+2) Fetch vs Axios
+3) UseEffect() hook 
--- pagination/src/App.js
+++ pagination/src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
-
+import {useEffect, useState} from 'react';
 function App() {
+
+  const [products,setProducts] = useState([]);
+
+  const fetchProducts = async () => {
+    const res = await fetch('https://dummyjson.com/products?limit=100');
+    const data = await res.json();
+
+    if(data && data.products){
+      setProducts(data.products)
+    }
+  };
+
+  console.log(products);
+
+  useEffect(() => {
+    fetchProducts();
+  }, []) 
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  
     </div>
   );
 }
